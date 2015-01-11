2b698170a5e87c19073ef15dc1f693a885b368e4
Pablo de la Peña <p@shedcollective.org>
2015-01-11 13:12:20 +0000

    Updated Formula
    Updated the formula to pull 2.0.5 release and checksum to match. Also added dependency checks for `composer` and `git-flow`

--- nails.rb
+++ nails.rb
@@ -2,8 +2,11 @@ require "formula"
 
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
-  url "https://github.com/nailsapp/command-line-tool/archive/0.1.0.tar.gz"
-  sha1 "c44db0cdfbaeeb017459bbfbcbfdbd7889db5507"
+  url "https://github.com/nailsapp/command-line-tool/archive/0.1.1.tar.gz"
+  sha1 "1f6f0dd2700e0755ecaf44a5f03f591a5135a76d"
+
+  depends_on "git-flow"
+  depends_on "composer"
 
   def install
     prefix.install "nails"
