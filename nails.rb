71c569978a469a7aef4b467178a050923f9bba8b
Pablo de la Peña <p@shedcollective.org>
2016-03-02 12:30:51 +0000

    Updated the formula to pull 0.6.1 release and checksum to match + dependancy on PHP

--- nails.rb
+++ nails.rb
@@ -2,12 +2,13 @@ require "formula"
 
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
-  url "https://github.com/nailsapp/command-line-tool/archive/0.6.0.tar.gz"
-  sha1 "7628f482e4690111f32517901a272a060a6febf5"
+  url "https://github.com/nailsapp/command-line-tool/archive/0.6.1.tar.gz"
+  sha1 "314e5f0b3b9ccc57bbc2410708e368178ddbabfb"
 
   depends_on "git-flow"
   depends_on "composer"
   depends_on "curl"
+  depends_on "php"
 
   def install
     prefix.install "nails"
