16fb6489a65fee0bf37370d27a85ef7df43baaf6
Pablo de la Peña <p@shedcollective.org>
2016-08-23 10:08:51 +0100

    Updated formula to use sha256

--- nails.rb
+++ nails.rb
@@ -3,7 +3,7 @@ require "formula"
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
   url "https://github.com/nailsapp/command-line-tool/archive/0.7.0.tar.gz"
-  sha1 "dac6919306731d638c4c2da71673a92da5f0b692"
+  sha256 "38675f144f85c3b99b5089263fb3e7e5e110a567fd9edc1ac176af0368484c17"
 
   depends_on "git-flow"
   depends_on "composer"
