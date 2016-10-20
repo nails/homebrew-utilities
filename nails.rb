77007959913bd8b1d0e41cd618aa400c48ad464b
Pablo de la Peña <p@shedcollective.org>
2016-10-20 15:42:23 +0100

    Corrected hash

--- nails.rb
+++ nails.rb
@@ -3,7 +3,7 @@ require "formula"
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
   url "https://github.com/nailsapp/command-line-tool/archive/0.7.0.tar.gz"
-  sha256 "38675f144f85c3b99b5089263fb3e7e5e110a567fd9edc1ac176af0368484c17"
+  sha256 "e3cff0501d98f01eeda0762c4de31754aecf3d596b3e73ef7aada790ad214f34"
 
   depends_on "git-flow"
   depends_on "composer"
