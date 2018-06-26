4a7b49a2622d42b4ac104738dd4f60f921a68ae6
Pablo de la Peña <p@shedcollective.org>
2018-06-27 00:27:26 +0100

    Updated the formula to pull 0.8.1 release

--- nails.rb
+++ nails.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
-  url "https://github.com/nailsapp/command-line-tool/archive/0.8.1.tar.gz"
+  url "https://github.com/nailsapp/command-line-tool/archive/0.9.0.tar.gz"
   sha256 "194de4a602461c0382e3156068d1c12f897fab6564939efb8e2b687723b61a05"
 
   depends_on "git-flow"
