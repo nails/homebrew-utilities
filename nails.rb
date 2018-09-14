7df8b7f7af06d9c57c55a093e38ddbe15d7e9c5b
Pablo de la Peña <p@shedcollective.org>
2018-09-14 14:53:15 +0100

    Corrected hash + added notes on generating correct hash

--- nails.rb
+++ nails.rb
@@ -3,7 +3,10 @@ require "formula"
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
   url "https://github.com/nails/command-line-tool/archive/0.10.2.tar.gz"
-  sha256 "a59ec839861aa5cecc5f1bd142f8d603d9f7500d78e09a2d6064f3a40b3f559c"
+
+  # Generate hash of the above file
+  # curl -sL https://github.com/nails/command-line-tool/archive/0.10.2.tar.gz | shasum -a 256
+  sha256 "10fc0857213c14882d0f271dd677183b89af98ce2b425ed605f0a39e92cd803b"
 
   depends_on "git-flow"
   depends_on "composer"
