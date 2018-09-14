cac5df2d3b6449df0e1317fde244d34dbacfcf52
Pablo de la Peña <p@shedcollective.org>
2018-09-14 15:18:36 +0100

    Updated the formula to pull 0.10.4 release and checksum to match.

--- nails.rb
+++ nails.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
-  url "https://github.com/nails/command-line-tool/archive/0.10.3.tar.gz"
+  url "https://github.com/nails/command-line-tool/archive/0.10.4.tar.gz"
 
   # Generate hash of the above file
-  # curl -sL https://github.com/nails/command-line-tool/archive/0.10.3.tar.gz | shasum -a 256
-  sha256 "bc5b029eacd94e35a0d41c6d7b17e13ee8d01ddadc9badcdc8f7c7db6d00df0e"
+  # curl -sL https://github.com/nails/command-line-tool/archive/0.10.4.tar.gz | shasum -a 256
+  sha256 "8c24bb715c32ba738efd376cdf5cae128279ce7bc8f88916816a318bb1ded4a3"
 
   depends_on "git-flow"
   depends_on "composer"
