a7a948dcf2746fff962b1dc51983d2669887d6c8
Pablo de la Peña <p@shedcollective.org>
2020-03-02 17:47:43 +0000

    Updated the formula to pull 1.2.4 release and checksum to match.

--- nails.rb
+++ nails.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Nails < Formula
   homepage "http://nailsapp.co.uk"
-  url "https://github.com/nails/command-line-tool/archive/1.2.3.tar.gz"
+  url "https://github.com/nails/command-line-tool/archive/1.2.4.tar.gz"
 
   # Generate hash of the above file
-  # curl -sL https://github.com/nails/command-line-tool/archive/1.2.3.tar.gz | shasum -a 256
-  sha256 "2e65053ca3c5f491d7bb0e7f42986b70fd00dc74d558f319b28a1c3c0781df57"
+  # curl -sL https://github.com/nails/command-line-tool/archive/1.2.4.tar.gz | shasum -a 256
+  sha256 "9cadcc307b7140d816e10c7e951448fb57ad3b78299368365d97bc98bf51550f"
 
   def install
     bin.install Dir["dist/*"]
