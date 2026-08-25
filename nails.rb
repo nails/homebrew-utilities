69bb2c4640dcb667879a0a98df58f54769698be0
Pablo de la Peña <p@shedcollective.org>
2026-08-25 14:21:50 +0100

    Updated the formula to pull 1.4.4 release and checksum to match.

--- nails.rb
+++ nails.rb
@@ -1,10 +1,10 @@
 Nails < Formula
   desc "Command Line tool for Nails"
   homepage "https://nailsapp.co.uk"
-  url "https://github.com/nails/command-line-tool/archive/1.4.3.tar.gz"
+  url "https://github.com/nails/command-line-tool/archive/1.4.4.tar.gz"
   # Generate hash of the above file
-  # curl -sL https://github.com/nails/command-line-tool/archive/1.4.3.tar.gz | shasum -a 256
-  sha256 "8d64d9ada3c362fe857247c6c35b34f427368d4871a4ef8b15c9d784a76f213f"
+  # curl -sL https://github.com/nails/command-line-tool/archive/1.4.4.tar.gz | shasum -a 256
+  sha256 "30c2107c7a286d3bd12fb154ddaaacb336330183442a2a56f0fee08aa82594ba"
   license "MIT"
 
   # Optional: Uncomment if you want Homebrew to ensure PHP is installed
@@ -18,6 +18,6 @@ Nails < Formula
 
   test do
     system "#{bin}/nails", "--version"
-    assert_match "Nails Command Line Tool 1.4.3", shell_output("#{bin}/nails --version")
+    assert_match "Nails Command Line Tool 1.4.4", shell_output("#{bin}/nails --version")
   end
 end
